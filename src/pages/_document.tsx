6a78a99b2aa555290e3ff4695c5e3b304735972c
Carter Himmel <me@fyko.net>
2023-06-11 23:13:51 -0600

    chore: move location of clarity script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default class CarterDocument extends Document {
 	public render() {
@@ -15,18 +14,6 @@ export default class CarterDocument extends Document {
 					<link
 						href="https://fonts.googleapis.com/css2?family=Krona+One&family=Roboto:wght@400;700&display=swap"
 						rel="stylesheet"
-					/>
-					<Script
-						strategy="afterInteractive"
-						dangerouslySetInnerHTML={{
-							__html: `
-							(function(c,l,a,r,i,t,y){
-								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-							})(window, document, "clarity", "script", "hikewdkjc0");
-						`,
-						}}
 					/>
 				</Head>
 				<body>
